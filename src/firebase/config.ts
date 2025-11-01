4f29cbd46292d3230ce69f144480d4f3fec21698
Tinku Sarkar <tinkusarkar.business@gmail.com>
2025-11-01 12:02:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-309973033-1ca91",
+  "appId": "1:617769747497:web:a2fe4015897d99d9e34838",
+  "apiKey": "AIzaSyBnz-YXuHnLtFb74tdXbKGV9LeDF9wLlFU",
+  "authDomain": "studio-309973033-1ca91.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "617769747497"
+};

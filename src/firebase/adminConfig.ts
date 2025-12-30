fd5d70e3ed5d16fbaa02acef4b9c5cddd70863fd
tinkusarkarbusiness-del <tinkusarkar.business@gmail.com>
2025-12-30 22:55:31 +0530

    Update adminConfig.ts
    import { initializeApp, cert, getApps } from "firebase-admin/app";
    
    export const firebaseAdminApp =
      getApps().length === 0
        ? initializeApp({
            credential: cert({
              projectId: process.env.FIREBASE_PROJECT_ID,
              clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
              privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
            }),
          })
        : getApps()[0];

--- src/firebase/adminConfig.ts
+++ src/firebase/adminConfig.ts
@@ -1,14 +1,12 @@
-import { initializeApp, getApps, cert } from "firebase-admin/app";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
 
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-};
-
-export const adminApp =
+export const firebaseAdminApp =
   getApps().length === 0
     ? initializeApp({
-        credential: cert(serviceAccount),
+        credential: cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        }),
       })
     : getApps()[0];

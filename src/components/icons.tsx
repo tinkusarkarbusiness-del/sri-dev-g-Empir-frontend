a904a26b75cdb4051e1d4d07f9238a56403d9bb4
tinkusarkarbusiness-del <tinkusarkar.business@gmail.com>
2026-01-07 22:47:56 +0530

    Update icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,6 +1,6 @@
 import type { SVGProps } from 'react';
 
-export function SriDevgLogo(props: SVGProps<SVGSVGElement>) {
+export function SriDevLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"

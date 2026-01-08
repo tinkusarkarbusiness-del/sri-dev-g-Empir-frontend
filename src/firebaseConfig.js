ba80b3e8f03237c7f92d81df9397ba3ed2f71c58
tinkusarkarbusiness-del <tinkusarkar.business@gmail.com>
2026-01-08 22:15:47 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,24 @@
-import { initializeApp } from "firebase/app";
+"use client";
+
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {        
-  apiKey: "AIzaSyBnz-YXuHnLtFb74tdXbKGV9LeDF9wLlFU",        
-  authDomain: "studio-309973033-1ca91.firebaseapp.com",        
-  projectId: "studio-309973033-1ca91",        
-  storageBucket: "studio-309973033-1ca91.appspot.com",        
-  messagingSenderId: "617769747497",        
-  appId: "1:617769747497:web:a2fe4015897d99d9e34838"        
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0
+  ? initializeApp(firebaseConfig)
+  : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-
+export { app };

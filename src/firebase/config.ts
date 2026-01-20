324c2861710c5b43071c0be5aec7a1d8d313c338
tinkusarkarbusiness-del <tinkusarkar.business@gmail.com>
2026-01-20 21:54:11 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { db } from '@/firebase/config';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {

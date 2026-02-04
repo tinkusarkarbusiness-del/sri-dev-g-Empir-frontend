c0641530815e36d02c391656e192ef22d957f01e
tinkusarkarbusiness-del <tinkusarkar.business@gmail.com>
2026-02-05 01:20:17 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,12 @@ import { cn } from '@/lib/utils'
 export const metadata = {
   title: "Sri Dev G Empire",
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
   },
 };
 

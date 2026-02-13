0f8c6e4fb43cd077b1c3e7371bfe5a10dabf89cb
tinkusarkarbusiness-del <tinkusarkar.business@gmail.com>
2026-02-13 22:53:47 +0530

    Update route.ts

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -56,6 +56,7 @@ if (email === OWNER_EMAIL) {
       secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
       path: "/",
+      domain: ".www.sridevgempire.com",
       maxAge: 60 * 60 * 24 * 5,
     });
 

9fde6acce780a567a8702ecb942595d3de1ba508
tinkusarkarbusiness-del <tinkusarkar.business@gmail.com>
2026-02-21 23:37:35 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,18 +2,15 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
+  const session = req.cookies.get("__session")?.value;
   const role = req.cookies.get("role")?.value;
-  const token = req.cookies.get("__session")?.value;
+
   const { pathname } = req.nextUrl;
 
-  // Not logged in
-  if (!token && pathname.startsWith("/admin")) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  // Admin protection
-  if (pathname.startsWith("/admin") && role !== "admin") {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
+  if (pathname.startsWith("/admin")) {
+    if (!session || role !== "admin") {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
   }
 
   return NextResponse.next();

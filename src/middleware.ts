fedc3b1b401dddffd174bb1c7f95d826a270203f
tinkusarkarbusiness-del <tinkusarkar.business@gmail.com>
2026-02-25 01:29:20 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,11 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { getAuth } from "firebase-admin/auth";
-import { firebaseAdminApp } from "@/firebase/adminConfig";
 
-export async function middleware(req: NextRequest) {
+export function middleware(req: NextRequest) {
   const session = req.cookies.get("__session")?.value;
-  const { pathname } = req.nextUrl;
 
-  if (pathname.startsWith("/admin")) {
+  if (req.nextUrl.pathname.startsWith("/admin")) {
     if (!session) {
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
-
-    try {
-      const decoded = await getAuth(firebaseAdminApp).verifyIdToken(session);
-
-      if (
-        decoded.role !== "admin" &&
-        decoded.role !== "superadmin"
-      ) {
-        return NextResponse.redirect(new URL("/login", req.url));
-      }
-    } catch (error) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
   }

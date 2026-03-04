d15b23341b5689838dd94e4d26eddeb629e377d8
tinkusarkarbusiness-del <tinkusarkar.business@gmail.com>
2026-03-04 16:03:18 +0530

    Change AuthRedirect to async function

--- src/app/auth-redirect/page.tsx
+++ src/app/auth-redirect/page.tsx
@@ -6,8 +6,6 @@ import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 import { firebaseAdminApp } from "@/lib/firebaseAdmin";
 
-export default function AuthRedirect() {
-  redirect("/dashboard");
 export default async function AuthRedirect() {
   const cookieStore = cookies();
   const session = cookieStore.get("__session")?.value;

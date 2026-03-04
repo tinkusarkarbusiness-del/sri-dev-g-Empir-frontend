3dd0d3b1b59456f2fa866f6a9bf1828931ab7798
tinkusarkarbusiness-del <tinkusarkar.business@gmail.com>
2026-03-04 15:51:13 +0530

    Change redirect path from '/admin/dashboard' to '/dashboard'

--- src/app/auth-redirect/page.tsx
+++ src/app/auth-redirect/page.tsx
@@ -3,5 +3,5 @@ export const dynamic = "force-dynamic";
 import { redirect } from "next/navigation";
 
 export default function AuthRedirect() {
-  redirect("/admin/dashboard");
+  redirect("/dashboard");
 }

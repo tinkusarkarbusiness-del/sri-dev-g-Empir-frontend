4ced95d154ef85a86fba17f7015b468b42db08db
tinkusarkarbusiness-del <tinkusarkar.business@gmail.com>
2026-03-04 14:42:24 +0530

    Change token verification to use session cookie

--- src/app/api/set-role/route.ts
+++ src/app/api/set-role/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    const decoded = await getAuth(firebaseAdminApp).verifyIdToken(session);
+    const decoded = await getAuth(firebaseAdminApp).verifySessionCookie(session);
 
     // ✅ Only superadmin can set roles
     if (decoded.role !== "superadmin") {

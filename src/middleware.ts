5471d5ff51dd7e796b9192302b2bdb1d67bfcc48
tinkusarkarbusiness-del <tinkusarkar.business@gmail.com>
2026-03-07 22:07:43 +0530

    Refactor session retrieval in middleware
    Updated session retrieval to include the cookie object.

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,16 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const session = req.cookies.get("__session")?.value;
 
+  const session = req.cookies.get("__session");
+
+  // protect admin routes
   if (req.nextUrl.pathname.startsWith("/admin")) {
+
     if (!session) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
+
   }
 
   return NextResponse.next();
